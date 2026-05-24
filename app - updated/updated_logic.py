3f5b866b26a26a2ff29e778747a060aa515cc14c
unknown <galburadumitru1990@gmail.com>
2026-05-24 15:18:44 +0100

    App

---
+++ app - updated/updated_logic.py
@@ -0,0 +1,11 @@
+class Task:
+
+    def __init__(self,title,notification_service=None):
+        self.title=title
+        self.notification_service=notification_service
+
+    def complete(self):
+        if self.notification_service:
+            self.notification_service.send(
+                f"Task completed:{self.title}"
+            )
